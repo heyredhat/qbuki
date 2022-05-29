dbcc3fbad1766e983145a7e0b2a26325e10a39d5
Matthew Weiss <heyredhat@gmail.com>
2022-05-29 01:19:52 -0400

    more

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ import setuptools
 
 setuptools.setup(
     name = "qbuki",
+    version = "0.0.1",
+    author = "Matthew Weiss",
+    author_email = "heyredhat@gmail.com",
+    description = "QBism for Python",
+    url = "https://github.com/heyredhat/qbuki",
     packages = setuptools.find_packages(),
     include_package_data = True)
-
