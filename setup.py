ca95ce0b80c2eec045ed08f092665af73e83d01c
Matthew Weiss <heyredhat@gmail.com>
2022-09-20 21:32:54 -0400

    wow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = "qbuki",
-    version = "0.0.9",
+    version = "0.1.0",
     author = "Matthew Weiss",
     author_email = "heyredhat@gmail.com",
     description = "QBism for Python",
